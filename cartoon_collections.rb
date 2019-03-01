633f9323fbf99df6902919d4fd379224d165fc53
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 23:22:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(calls_long)# code an argument here
-  words.all? { |word| word.length >= 4 }
+  calls_long.all? { |word| word.length >= 4 }
   # Your code here
 end
 
