faa7f151c8ca58e60012b2fb56eed96d09238f08
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 23:31:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,10 @@ def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?(cheese_types) {|cheese|}
+    return true
+  else nil
 end
