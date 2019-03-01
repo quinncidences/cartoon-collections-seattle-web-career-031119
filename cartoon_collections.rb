6cddcb810d673e99768d4e59a7e00b36701069b5
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 23:35:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,6 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types) {|cheese|}
-    return array.findFirst(cheese_types)
-  else nil
-end
+  array.findFirst do |cheese|
+  array.include?(cheese_types)
 end
