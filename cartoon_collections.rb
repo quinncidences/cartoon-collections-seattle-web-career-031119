442278c13e6c82b8f248a9dd0ab187739b14bd2b
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 23:09:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,5 @@
 array = ["Doc", "Dopey", "Bashful", "Grumpy", "Happy", "Sleepy", "Sneezy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def roll_call_dwarves(array)# code an argument here
   i = 1
@@ -8,7 +9,8 @@ def roll_call_dwarves(array)# code an argument here
   end# Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.map! {|name| name.capitalize}
   # Your code here
 end
 
